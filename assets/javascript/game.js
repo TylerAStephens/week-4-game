9855e80bb3d6a816cbcf3f83868b985a7dcabd92
TylerAStephens <tylerstephens16@yahoo.com>
2017-08-26 21:03:34 -0400

    added random number

---
+++ assets/javascript/game.js
@@ -0,0 +1,25 @@
+// generate random number
+var myRandom = randomRange(19,120);
+
+function randomRange(myMin, myMax) {
+    return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
+   
+}
+console.log(myRandom);
+// add 4 buttons
+
+// add random values to each button
+
+// if clicked on add values to a player score
+
+// player wins if score is matched
+
+// player loses if score goes over
+
+//add alerts to each win or lose
+
+// add win and lose to win and loss score
+
+// reset random numener but keep score log
+
+// add reset button to reset all scores
